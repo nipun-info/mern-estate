ed8382d9991fbf0d035c87855fd6a27090738bcf
Nipun Shah <nasihul2010@gmail.com>
2023-11-19 10:05:38 +0600

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold underline'>Real states</h2>
+    </div>
+  );
+}
+
+export default App
